34f23b2303ec05fad7ccbc84a196e374be3b0852
Barath-N-P <barathnatarajan234@gmail.com>
2025-12-24 20:47:39 +0530

    Add files via upload

---
+++ assesment 3/p10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{  
+  int a;
+  printf("Enter the number:");
+  scanf("%d",&a);
+  if((a%10)>(a/10))
+  {
+    printf("Success\n");
+  }
+  else
+  {
+    printf("Failure\n");
+  }
+
+}
